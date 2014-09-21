bd45ff93127c62f91a858951ed1b8e145d44af51
cternes <github@slackspace.de>
2014-09-21 09:24:24 +0200

    Implemented basic mechanics

--- js/controllers.js
+++ js/controllers.js
@@ -28,17 +28,52 @@ xmaspicsControllers.controller('SplashController', function ($scope) {
 });
 
 xmaspicsControllers.controller('MemoryController', function ($scope) {
+	$scope.score = {"points": 0, "attempts": 0};
+	$scope.openCards = [];
+	
 	$scope.cards = 
-		[ {"id": "1", "imgDeck": "img/01/card_hidden.jpg", "img": "img/01/1.jpg", "deckVisible": true},
-		  {"id": "2", "imgDeck": "img/01/card_hidden.jpg", "img": "img/01/2.jpg", "deckVisible": true},
-		  {"id": "3", "imgDeck": "img/01/card_hidden.jpg", "img": "img/01/1.jpg", "deckVisible": true},
-		  {"id": "4", "imgDeck": "img/01/card_hidden.jpg", "img": "img/01/2.jpg", "deckVisible": true}
+		[ {"id": "1", "imgDeck": "img/01/card_hidden.jpg", "img": "img/01/1.jpg", "deckVisible": true, "canFlipped": true},
+		  {"id": "2", "imgDeck": "img/01/card_hidden.jpg", "img": "img/01/2.jpg", "deckVisible": true, "canFlipped": true},
+		  {"id": "3", "imgDeck": "img/01/card_hidden.jpg", "img": "img/01/1.jpg", "deckVisible": true, "canFlipped": true},
+		  {"id": "4", "imgDeck": "img/01/card_hidden.jpg", "img": "img/01/2.jpg", "deckVisible": true, "canFlipped": true}
 		];
 	
 	$scope.flipCard = function(card) {
+		//do nothing if card cannot be flipped
+		if(!card.canFlipped) {
+			return;
+		}
+		
 		//toggle deckVisible property
 		card.deckVisible = card.deckVisible ? false : true;
+		
+		//add current cards to openCards
+		$scope.openCards.push(card);
+		
+		//when two cards are open, check if they match
+		if($scope.openCards.length == 2) {
+			checkMatchingCards();
+		}
 	}
 	
+	function checkMatchingCards() {
+		//if cards match, mark cards as not flippable and increase points
+		if($scope.openCards[0].img === $scope.openCards[1].img) {
+			$scope.openCards[0].canFlipped = false;
+			$scope.openCards[1].canFlipped = false;
+			
+			$scope.score.points++;
+		}
+		else {
+			$scope.openCards[0].deckVisible = true;
+			$scope.openCards[1].deckVisible = true;
+		}
+		
+		//always increase attempts
+		$scope.score.attempts++;
+		
+		//always clear openCards 
+		$scope.openCards = [];
+	}
 	
 });
