f7001ec30751d0eb5eaf2b074b75ca3f94fbc8ee
cternes <github@slackspace.de>
2014-10-05 08:34:02 +0200

    Linked splash with memory

--- js/controllers.js
+++ js/controllers.js
@@ -9,22 +9,6 @@ xmaspicsControllers.controller('SplashController', function ($scope) {
 							fontSize: "150px",
 							ease:Bounce.easeOut});
 	
-	$scope.showSlideshow = false;
-	$scope.startSlideshow = function() {
-		$scope.showSlideshow = true;
-			
-		//start zoom animation
-		var image1 = document.getElementById("p1");
-		var image2 = document.getElementById("p2");
-		
-		var tl = new TimelineLite({});
-		tl.to(image1, 5, {scale: 2, ease: Linear.easeNone})
-		  .to(image2, 5, {scale: 2, ease: Linear.easeNone}, "-=1.0");
-		
-		var tl2 = new TimelineLite({});
-		tl2.to(image1, 0.3, {opacity: 0, delay: 4.5, ease: Linear.easeNone});
-	}
-	
 });
 
 xmaspicsControllers.controller('MemoryController', function ($scope) {
